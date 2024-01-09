f6857c333d2ecf7b5f71ce2250f73ab0e1d346c6
ProgSamuel <progsamu@gmail.com>
2024-01-08 22:24:08 -0300

    middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from 'cors';
 import { UserController } from "./Controllers/user.controller";
 import { TwitterController } from "./Controllers/twitter.controller";
+import { checkId } from "./middlewares/checkId";
 
 const app = express();
 app.use(express.json());
@@ -12,8 +13,8 @@ const twitterController = new TwitterController()
 app.post('/createuser', userController.createUser) 
 // fazer o MIDD
 app.post('/user/login', userController.login)
-app.get('/user/:idUser', userController.searcheUser)
-app.post('/user/:idUser/createtwitter', twitterController.createTwitter )
+app.get('/user/:idUser', checkId, userController.searcheUser)
+app.post('/user/:idUser/createtwitter', checkId,twitterController.createTwitter )
 
 app.listen(3335, () => {
     console.log("A API está rodando!");
--- src/middlewares/checkId.ts
+++ src/middlewares/checkId.ts
@@ -1,11 +1,40 @@
 import { Request, Response } from "express";
+import { User } from "../Models/user.model";
+import repository from "../database/prisma.repository";
+import { notFound, serverError } from "../Utils/response.helper";
 
-export const checkId = (req:Request, res:Response, next: () => void) => {
+export const checkId = async (req: Request, res: Response, next: () => void) => {
+    try {
+        const { idUser } = req.params;
+        const { authorization } = req.headers;
 
-    // check ID
-    
+        // checked authorization
+        if (!authorization) {
+            return res.status(401).send({
+                ok: false,
+                message: "Authentication token not provided",
+            });
+        }
 
+        // search user 
+        const user = await repository.user.findUnique({
+            where: { idUser },
+        });
 
-    next()
+        // checked id
+        if (!user) {
+            return notFound(res, "User");
+        }
 
-}
+        if (authorization !== user.token) {
+            return res.status(401).send({
+                ok: false,
+                message: "Unauthorized access",
+            });
+        }
+
+        next();
+    } catch (error) {
+        return serverError(res, error);
+    }
+};
